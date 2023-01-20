9fd3aa2b25f9000ad84d0b7f0df42ab8336c0c5d
DDELS <51665466+DDELS@users.noreply.github.com>
2023-01-20 04:48:19 -0500

    Trying to add Geo-location with Country & City

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//David Delgado
+const temp =[];
 
 function hideDivDisplay(id){
     let targetDiv = document.getElementById(id);
@@ -10,21 +12,81 @@ function hideDivDisplay(id){
     
 }
 
-function getLocation(){
-    const location = document.getElementById("locationContent");
-    console.log(location);
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showLocation);
-        console.log(navigator.geolocation.getCurrentPosition(showLocation));
-    }else{
-        location.innerHTML = "Geolocation is not supported by this browser.";
-    }
-    //console.log(location);
+
+const options = {
+    enableHighAccuracy: true,
+};
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-function showLocation(position){
-    location.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; 
+async function getFile(url){
+    let file = await fetch(url);
+    let fileText = await file.text();
+    let data = JSON.parse(fileText);
+    // data[0] = data.localityInfo.administrative[0].name;
+    // data[1] = data.localityInfo.administrative[1].name;
+    // data[2] = data.localityInfo.administrative[2].name;
+    //console.log(data);
+    //console.log(data.localityInfo.administrative[0].name);
+    //console.log(data.localityInfo.administrative[1].name);
+    //console.log(data.localityInfo.administrative[2].name);
+   // location.innerHTML += "<br>Country: " + data.localityInfo.administrative[0].name + "<br>State: " + data.localityInfo.administrative[1].name + "<br>City/County: " + data.localityInfo.administrative[2].name; 
+    temp[0] = data.localityInfo.administrative[0].name;
+    temp[1] = data.localityInfo.administrative[1].name;
+    temp[2] = data.localityInfo.administrative[2].name;
+    //location.innerHTML += "<br>Country: " + temp[0] + "<br>State: " + temp[1] + "<br>City/County: " + temp[2]; 
+    return data ;
 }
+
+function success(pos) {
+    let location = document.getElementById("locationContent");
+    const crd = pos.coords;
+    let latitude = crd.latitude;
+    let longitude = crd.longitude;
+    let accuracy = crd.accuracy;
+    
+    // console.log('Your current position is:');
+    // console.log(`Latitude : ${crd.latitude}`);
+    // console.log(`Longitude: ${crd.longitude}`);
+    // console.log(`More or less ${crd.accuracy} meters.`);
+
+    location.innerHTML = "Latitude : " + latitude + " Longitude: " + longitude + "<br> Accuracy: " + accuracy + " meter(s)";
+    var dataUrl = "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=" +latitude+"&longitude="+longitude+"&localityLanguage=en";
+   
+    //getFile(dataUrl);
+    var Promise = getFile(dataUrl);
+    Promise.then(function (val){
+        console.log(val); 
+    });
+    console.log("data...");
+    //console.log(getFile(dataUrl));
+    
+
+    
+   
+
+}
+
+
+  
+function getLocation(){ 
+    if(success){
+        navigator.geolocation.getCurrentPosition(success, error, options); 
+        
+    }else{
+        error(err);
+    }
+        
+    
+       
+   
+
+}
+
+
+
 
 
 
