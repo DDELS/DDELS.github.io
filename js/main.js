ee57999c49b682b1f066c5e6002d620496dbf9d3
DDELS <51665466+DDELS@users.noreply.github.com>
2023-01-19 22:31:30 -0500

    Testing geo-location

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 
 function hideDivDisplay(id){
     let targetDiv = document.getElementById(id);
-    if(targetDiv.style.display != "none"){
-        targetDiv.style.display = "none";
-    }else{
-        targetDiv.style.display= "block";
+    //console.log(targetDiv.style.display);
+    if(targetDiv.style.display == "" || targetDiv.style.display == "none" ){
+        targetDiv.style.display = "block";
+    }else {
+        targetDiv.style.display= "none";
     }
-
+    
 }
+
+function getLocation(){
+    const location = document.getElementById("locationContent");
+    console.log(location);
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showLocation);
+        console.log(navigator.geolocation.getCurrentPosition(showLocation));
+    }else{
+        location.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    //console.log(location);
+}
+
+function showLocation(position){
+    location.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; 
+}
+
+
+
+
 
 
 
