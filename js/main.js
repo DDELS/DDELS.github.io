d4adaf3cb1658c68e392f1ea877e4d9353c7eae9
DDELS <51665466+DDELS@users.noreply.github.com>
2023-01-19 19:21:56 -0500

    Changed js file titled, added hide function

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+function hideDivDisplay(id){
+    let targetDiv = document.getElementById(id);
+    if(targetDiv.style.display != "none"){
+        targetDiv.style.display = "none";
+    }else{
+        targetDiv.style.display= "block";
+    }
+
+}
+
+
+
+
+
