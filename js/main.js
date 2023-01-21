92d020f8e9a697a73e4d2ee6b243db7c608318e3
DDELS <51665466+DDELS@users.noreply.github.com>
2023-01-21 06:19:40 -0500

    cleaned up code and added an async function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //David Delgado
-let temp =[];
 
 function hideDivDisplay(id){
     let targetDiv = document.getElementById(id);
@@ -21,96 +20,26 @@ function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
+function getLocation(){ 
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
 
-
-
-// async function getFile(url){
-//     const file = await fetch(url)
-//      //await fetch(url).then((response) => response.json()).then((data) => temp = data );
-        
-//     let fileText = await file.text();
-//     let data = JSON.parse(fileText);
-
-//     //localStorage.setItem("locationData", data);
-
-//     // data[0] = data.localityInfo.administrative[0].name;
-//     // data[1] = data.localityInfo.administrative[1].name;
-//     // data[2] = data.localityInfo.administrative[2].name;
-//     //console.log(data);
-//     //console.log(data.localityInfo.administrative[0].name);
-//     //console.log(data.localityInfo.administrative[1].name);
-//     //console.log(data.localityInfo.administrative[2].name);
-//    // location.innerHTML += "<br>Country: " + data.localityInfo.administrative[0].name + "<br>State: " + data.localityInfo.administrative[1].name + "<br>City/County: " + data.localityInfo.administrative[2].name; 
-//     // temp[0] = data.localityInfo.administrative[0].name;
-//     // temp[1] = data.localityInfo.administrative[1].name;
-//     // temp[2] = data.localityInfo.administrative[2].name;
-    
-
-//     ///console.log(temp);
-//     //location.innerHTML += "<br>Country: " + data.localityInfo.administrative[0].name + "<br>State: " + data.localityInfo.administrative[1].name + "<br>City/County: " + data.localityInfo.administrative[2].name;
-//     console.log("Done...")
-    
-//     return data;
-    
-// }
-
-function success(pos) {
+async function success(pos) {
     let location = document.getElementById("locationText");
     const crd = pos.coords;
     let latitude = crd.latitude;
     let longitude = crd.longitude;
     let accuracy = crd.accuracy;
-    
-    // console.log('Your current position is:');
-    // console.log(`Latitude : ${crd.latitude}`);
-    // console.log(`Longitude: ${crd.longitude}`);
-    // console.log(`More or less ${crd.accuracy} meters.`);
-
-    location.innerHTML = "<b>Latitude : </b>" + latitude + "      <b>Longitude: </b>" + longitude + "<br> <b>Accuracy: </b>" + accuracy + " meter(s)";
     var dataUrl = "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=" +latitude+"&longitude="+longitude+"&localityLanguage=en";
-    
-    // getFile(dataUrl).then(
-    //     function(value){
-    //           //location.innerHTML += "<br>Country: " + temp + "<br>State: " + temp.principalSubdivision + "<br>City/County: " + temp.locality; 
-    //         displayLocation(value);
-    //     }
-    // )
-    //temp = localStorage.getItem("locationData");
-   // location.innerHTML += "<br>Country: " + temp + "<br>State: " + temp.principalSubdivision + "<br>City/County: " + temp.locality; 
 
+    location.innerHTML = "<b>Latitude/Longitude: </b>" + latitude + " / " + longitude + "<br> <b>Accuracy: </b>" + accuracy + " meter(s)";    
     
-    
-    
-    // getFile(dataUrl);
-    // Promise.then(function (val){
-    //     console.log(val); 
-    // });
-    // console.log("data...");
-    //console.log(temp);
-    
-    //console.log(getFile(dataUrl));
-    
-    // getFile(dataUrl) = new Promise(function(myResolve, myReject){
-    //     if(success){
-    //         myResolve(); 
-            
-    //         //console.log(temp);
-    //     }else{
-    //         myReject(error(err));
-    //     }
-    // });
-    
-    // getFile(dataUrl).then(
-    //     function(value){displayLocation(value);},
-    //     function(error){displayLocation(error);}
-    // );
-
-    let myPromise = new Promise(function(myResolve, myReject){
+    let getJSON = new Promise(function(myResolve, myReject){
         let req = new XMLHttpRequest();
         req.open('GET',dataUrl);
         req.onload = function(){
             let temp = JSON.parse(req.response);
-            //console.log(temp.continent);
+            //console.log(temp);
             if(req.status == 200){
                 myResolve(temp);
             }else{
@@ -120,9 +49,8 @@ function success(pos) {
         req.send();
     });
 
-    myPromise.then(
+    getJSON.then(
        function(value){
-        
         displayLocation(value);
        }, 
        function(error){
@@ -131,59 +59,13 @@ function success(pos) {
     )
     
 
-
-
-
-
-
-
-    
-   
-
 }
-
-
-
   
-function getLocation(){ 
-    
-    // var myPromise = new Promise(function(myResolve, myReject){
-    //     if(success){
-    //         myResolve(getFile(dataUrl) ); 
-            
-    //         //console.log(temp);
-    //     }else{
-    //         myReject(error(err));
-    //     }
-    // });
-    
-    // myPromise.then(
-    //     function(value){displayLocation(value);},
-    //     function(error){displayLocation(error);}
-    // );
-    
-    navigator.geolocation.getCurrentPosition(success, error, options);
-    
-    
-    // if(success){
-    //     navigator.geolocation.getCurrentPosition(success, error, options); 
-        
-    //     //console.log(temp);
-    // }else{
-    //     error(err);
-    // }
-        
-    
-       
-   
-
-}
-
 function displayLocation(data){
-    //navigator.geolocation.getCurrentPosition(success, error, options);
     let location = document.getElementById("locationText");
     console.log(data);
-    location.innerHTML += "<br><b>Country:</b> "+ data.countryCode + "<br><b>State:</b> " + data.principalSubdivision + "<br><b>City/County: </b>" + data.locality + "<br><b>Zip-Code: </b>" + data.postcode + "</br>";
+    
+    location.innerHTML += "<br><b>Country:</b> "+ data.countryName +" (" + data.countryCode + ")" + "<br><b>State:</b> " + data.principalSubdivision + "<br><b>City: </b>"+ data.city + "<br><b>County: </b>" + data.locality + "<br><b>Zip-Code: </b>" + data.postcode + "</br>";
 }
 
 
